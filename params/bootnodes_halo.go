d800c869c793c375a8697be8e4547ace1e60b124
halo-intern <sujidev550@gmail.com>
2025-10-26 14:50:29 -0700

    halo bootnode

--- params/bootnodes_halo.go
+++ params/bootnodes_halo.go
@@ -8,6 +8,7 @@ var HaloBootnodes = []string{
 	// Example:
 	// "enode://abcd1234...@203.0.113.1:30303",
 	// "enode://efgh5678...@203.0.113.2:30303",
+	"enode://67b03fc4c109abd319ee7eb156dd0ce25c61ccb07848810fa4142cee50d28e505812f68b7ee5a5260f6904bb0fe7edb37ce0a5af2671206fc1930ae24dbdc22b@51.20.84.193:30301"
 }
 
 // HaloDNSNetwork is the DNS discovery configuration for Halo network
